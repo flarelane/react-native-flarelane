4811e2d5b012108e989905da7447733b33548b05
MinHyeok Kim <minhyeok4dev@gmail.com>
2025-07-14 19:19:36 +0900

    Update SDK

--- ios/RCTFlareLane.swift
+++ ios/RCTFlareLane.swift
@@ -12,7 +12,7 @@ class RCTFlareLane: RCTEventEmitter {
   override init() {
     super.init()
     RCTFlareLane.emitter = self
-    FlareLane.setSdkInfo(sdkType: .reactnative, sdkVersion: "1.9.0")
+    FlareLane.setSdkInfo(sdkType: .reactnative, sdkVersion: "1.9.2")
   }
 
   // ----- PUBLIC METHODS -----
